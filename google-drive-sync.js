884e044e6643998c9abcf2c539793397c1f44e37
wcaactivities <wcaactivities@gmail.com>
2026-05-04 16:51:12 -0400

    Add files via upload

--- google-drive-sync.js
+++ google-drive-sync.js
@@ -250,21 +250,32 @@ class GoogleDriveSync {
                     'application/json'
                 );
             } else {
-                // Create new file
+                // Create new file using multipart upload
+                const boundary = '-------314159265358979323846';
+                const delimiter = "\r\n--" + boundary + "\r\n";
+                const close_delim = "\r\n--" + boundary + "--";
+
                 const metadata = {
                     name: fileName,
                     parents: [this.folderId],
                     mimeType: 'application/json'
                 };
 
+                const multipartRequestBody =
+                    delimiter +
+                    'Content-Type: application/json\r\n\r\n' +
+                    JSON.stringify(metadata) +
+                    delimiter +
+                    'Content-Type: application/json\r\n\r\n' +
+                    fileContent +
+                    close_delim;
+
                 response = await this.makeApiCall(
                     'POST',
                     'https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart',
                     null,
-                    {
-                        metadata: metadata,
-                        content: fileContent
-                    }
+                    multipartRequestBody,
+                    'multipart/related; boundary=' + boundary
                 );
                 
                 this.fileId = response.id;
